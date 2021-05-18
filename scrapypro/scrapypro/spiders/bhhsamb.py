958567e0badaa0cdea65c55d34ad960b75564853
anisha7025 <anisha7025@gmail.com>
2021-05-18 21:50:02 +0530

    commit

--- scrapypro/scrapypro/spiders/bhhsamb.py
+++ scrapypro/scrapypro/spiders/bhhsamb.py
@@ -10,26 +10,25 @@ class BhhsambSpider(scrapy.Spider):
     page_number = 2
 
     def parse(self, response):
-        soup = bs(response.text)
-        links = soup.findAll('span', attrs={'class': 'agent-name'})
-        for link in links:
-            # print(self.start_urls[0]+link.a['href'][8:])
-            yield scrapy.Request(self.start_urls[0] + link.a['href'][8:], callback=self.parse_details)
+        all_links = response.xpath("//div[@class='col-md-8 col-sm-12']")
+        for link in all_links:
+            url = link.xpath(".//div[@class='agent-info']/span/a/@href").get()
+            # print(self.start_urls[0]+url[8:])
+            yield scrapy.Request(self.start_urls[0] + url[8:], callback=self.parse_details)
 
-    def parse_details(self, response):
+
+def parse_details(self, response):
 
         divs = response.xpath("//div[@class='agent-details col-sm-24 kill-padding']")
 
         name = divs.xpath("//div[@class='row']/h1/text()").get()
-        job_title = divs.xpath(
-            "//div[@class='text-left medium-text mobile-text-center']/span[@class='big-text']/text()").get()
+        job_title = divs.xpath("//div[@class='text-left medium-text mobile-text-center']/span[@class='big-text']/text()").get()
         image_url = divs.xpath("//img[@class='agent-photo']/@src").get()
         address = divs.xpath("//div[@class='row']/div[@class='text-left medium-text mobile-text-center']//text()").get()
 
         office = divs.xpath("//a[@data-type='Office']/text()").get()
         cell = divs.xpath("//a[@data-type='Agent']/text()").get()
-        email = divs.xpath(
-            "//div[@class='text-left medium-text mobile-text-center']/a[@class='agent_email']/@href").get()
+        email = divs.xpath("//div[@class='text-left medium-text mobile-text-center']/a[@class='agent_email']/@href").get()
 
         fb = divs.xpath("//div[@class='agent-social-icons social']/a[@class='fb']/@href").get()
         tw = divs.xpath("//div[@class='agent-social-icons social']/a[@class='tw']/@href").get()
@@ -40,7 +39,8 @@ class BhhsambSpider(scrapy.Spider):
 
         offices = divs.xpath("//div[@id='team_offices']/a/text()").extract()
         languages = divs.xpath("//div[@class='language-list']/ul/li/text()").extract()
-        description = response.xpath("//div[@class='col-sm-24']//following-sibling::p//text()").extract()
+        description = response.xpath("//div[@class='col-sm-24']//following-sibling::p//text()").get()
+
 
         yield {
             'name': name,
@@ -65,6 +65,7 @@ class BhhsambSpider(scrapy.Spider):
             'languages': languages,
             'description': description
         }
+
 
         new_page = 'https://www.bhhsamb.com/agents?page=' + str(BhhsambSpider.page_number)
         if BhhsambSpider.page_number < 43:
